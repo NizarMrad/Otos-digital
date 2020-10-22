709f97bf18fc30722cc366d18cddc730f0611e2a
NizarMrad <nizar.mrad.pro@gmail.com>
2020-10-22 18:00:42 +0200

    first upload

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,42 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+//= require jquery-3.2.1.min
+//= require nile-slider
+//= require scrolla.jquery.min
+//= require assets/rslider/js/jquery.themepunch.tools.min
+//= require assets/rslider/js/jquery.themepunch.revolution.min
+//= require assets/rslider/js/extensions/revolution.extension.actions.min
+//= require assets/rslider/js/extensions/revolution.extension.carousel.min
+//= require assets/rslider/js/extensions/revolution.extension.kenburn.min
+//= require assets/rslider/js/extensions/revolution.extension.layeranimation.min
+//= require assets/rslider/js/extensions/revolution.extension.migration.min
+//= require assets/rslider/js/extensions/revolution.extension.navigation.min
+//= require assets/rslider/js/extensions/revolution.extension.parallax.min
+//= require assets/rslider/js/extensions/revolution.extension.slideanims.min
+//= require assets/rslider/js/extensions/revolution.extension.video.min
+//= require YouTubePopUp.jquery
+//= require owl.carousel.min
+//= require imagesloaded.min
+//= require popper.min
+//= require bootstrap.min
+//= require imagesloaded.min
+//= require jquery.filterizr.min
+//= require wow.min
+//= require custom
+
+document.addEventListener("turbolinks:load", function () {});
